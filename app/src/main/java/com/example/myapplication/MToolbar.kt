5bd374b35773ad0c3e77018279f77196ab91d97a
Lucas Militão <lucasmengineer@edu.unifor.br>
2020-01-14 11:34:58 -0300

    first commit

---
+++ app/src/main/java/com/example/myapplication/MToolbar.kt
@@ -0,0 +1,176 @@
+package com.example.myapplication
+
+import android.content.Context
+import android.os.Build
+import android.text.TextUtils
+import android.util.AttributeSet
+import android.view.Gravity
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.marginStart
+
+class MToolbar : Toolbar {
+
+    private lateinit var _title: TextView
+    private lateinit var _view: RelativeLayout
+
+    private var toolbarViewHeightSize: Int = 0
+    private var viewWidthSize: Int = 0
+    private var toolbarHeightSize: Int = 0
+    private var toolbarWidhtSize: Int = 0
+    private var middleToolbar: Int = 0
+    private var middleToolbarView: Int = 0
+    private var spaceLeftView: Int = 0
+    private var spaceRightView: Int = 0
+
+    constructor(context: Context?) : super(context, null)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+    init {
+        getTitleTextView()
+
+//        getTitleTextView().viewTreeObserver.addOnGlobalLayoutListener(object :
+//            ViewTreeObserver.OnGlobalLayoutListener {
+//            override fun onGlobalLayout() {
+//
+//                val view = this@MToolbar
+//                print("acabou")
+//
+//                val array = intArrayOf(0, 0)
+//                view.getTitleTextView().getLocationInWindow(array)
+//                array
+//
+//                spaceLeftTitle = middleToolbar - array[1]
+//                spaceRightTitle = toolbarTextViewWidthSize - spaceLeftTitle
+//                print("")
+//
+//                getTitleTextView().apply {
+//                    val lp = layoutParams.apply {
+//                        width = spaceRightTitle * 2
+//                    }
+//                    layoutParams = lp
+//                }
+//
+//                getTitleTextView().viewTreeObserver.removeOnGlobalLayoutListener(this)
+//            }
+//        })
+
+    }
+
+    override fun getTitle(): CharSequence {
+        return getTitleTextView().text
+    }
+
+    override fun setTitle(title: CharSequence?) {
+        getTitleTextView().apply {
+            text = title
+        }.requestLayout()
+    }
+
+    override fun setTitle(resId: Int) {
+        getTitleTextView().apply {
+            setText(resId)
+        }.requestLayout()
+    }
+
+    private fun getTitleTextView(): TextView {
+
+        if (!::_title.isInitialized) {
+
+            _view = RelativeLayout(context).also { v ->
+
+                val lp = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT).also {
+                    it.gravity = Gravity.CENTER
+                }
+
+                v.layoutParams = lp
+
+                addView(v)
+            }
+
+            _title = ToolbarTitleTextView(context).also { tv ->
+
+                tv.setSingleLine()
+                tv.ellipsize = TextUtils.TruncateAt.END
+                tv.gravity = Gravity.CENTER
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    tv.setTextAppearance(R.style.TextAppearance_AppCompat_Widget_ActionBar_Title)
+                } else {
+                    tv.setTextAppearance(
+                        context,
+                        R.style.TextAppearance_AppCompat_Widget_ActionBar_Title
+                    )
+                }
+
+                val lp = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT).also {
+                    it.gravity = Gravity.CENTER
+                }
+
+                tv.layoutParams = lp
+
+                _view.addView(tv)
+            }
+        }
+
+        return _title
+    }
+
+//    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+//        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+//
+//        toolbarHeightSize = measuredHeight
+//        toolbarWidhtSize = measuredWidth
+//        middleToolbar = toolbarWidhtSize / 2
+//    }
+
+    inner class ToolbarTitleTextView(context: Context?) : TextView(context) {
+
+        override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+            toolbarWidhtSize = this@MToolbar.measuredWidth
+            viewWidthSize = this@MToolbar._view.measuredWidth
+            middleToolbar = toolbarWidhtSize / 2
+            middleToolbarView = viewWidthSize / 2
+
+            val array = intArrayOf(0, 0)
+            this@MToolbar._view.getLocationInWindow(array)
+
+            spaceLeftView = middleToolbar - array[0]
+            spaceRightView = viewWidthSize - spaceLeftView
+
+            val lp = (this.layoutParams as RelativeLayout.LayoutParams).apply  {
+                if(spaceLeftView > spaceRightView) {
+                    setMeasuredDimension(spaceRightView * 2, MeasureSpec.getSize(heightMeasureSpec))
+                    setMargins(middleToolbar - (this@ToolbarTitleTextView.measuredWidth / 2) - array[0], 0, 0, 0)
+                } else {
+                    setMeasuredDimension(spaceLeftView * 2, MeasureSpec.getSize(heightMeasureSpec))
+                    setMargins(0, 0, middleToolbar - (this@ToolbarTitleTextView.measuredWidth / 2) - array[0], 0)
+                }
+            }
+
+            layoutParams = lp
+
+//
+//            if (layoutParams.width == ViewGroup.LayoutParams.MATCH_PARENT) {
+//                val lp = layoutParams.apply {
+//                    width = spaceRightTitle * 2
+//                }
+//                layoutParams = lp
+//                requestLayout()
+//            }
+
+
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.myapplication
+
+import android.animation.Animator
+import android.app.SearchManager
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextWatcher
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewAnimationUtils
+import android.view.animation.AnimationUtils
+import android.view.animation.DecelerateInterpolator
+import android.widget.SearchView
+import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun openSearchView() {
+        search_input_text.setText("")
+        search_open_view.visibility = View.VISIBLE
+        val circularReveal = ViewAnimationUtils.createCircularReveal(
+            search_open_view,
+
+        )
+    }
+}
